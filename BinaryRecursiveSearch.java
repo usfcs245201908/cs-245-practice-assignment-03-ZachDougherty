609274229c02b3ceb86c9f84247ffefb201404d4
Zachary Dougherty <babyguraffe@gmail.com>
2019-09-06 13:56:08 -0700

    implemented three different searches

---
+++ BinaryRecursiveSearch.java
@@ -0,0 +1,30 @@
+
+
+public class BinaryRecursiveSearch implements Practice03Search {
+
+    public String searchName() {
+        return "Binary recursive search";
+    }
+
+    public int search(int[] arr, int target) {
+       return recSearch(arr, target, 0, arr.length-1);
+    }
+
+    private int recSearch(int[] arr, int target, int low, int high) {
+        int mid;
+        if (low > high) {
+            return -1;
+        } else {
+            mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (mid > target) {
+                high = mid -1;
+                return recSearch(arr, target, low, high);
+            } else {
+                low = mid + 1;
+                return recSearch(arr, target, low, high);
+            }
+        }
+    }
+}
